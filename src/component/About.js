1e12bbe5061f77efb5322d729df20f2fd3c671a6
Pramod <pramodthakur9991@gmail.com>
2021-12-23 16:57:23 +0530

    added

---
+++ src/component/About.js
@@ -0,0 +1,48 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <div className='container'>
+            <h1 className='my-3'>About us</h1>
+            <div className="accordion" id="accordionExample">
+                <div className="accordion-item">
+                    <h2 className="accordion-header" id="headingOne">
+                        <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            Analyze your text
+                        </button>
+                    </h2>
+                    <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                        <div className="accordion-body">
+                    TextUtils gives you a way to analyze your text quickly and efficiently. 
+                        </div>
+                    </div>
+                </div>
+                <div className="accordion-item">
+                    <h2 className="accordion-header" id="headingTwo">
+                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                            Free to use
+                        </button>
+                    </h2>
+                    <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                        <div className="accordion-body">
+                     TextUtils is a free character counter tool that provides instant character count and word count statistics for a given text. TextUtils reports the number of words and characters. Thus it is suitable for writing text with word/character limit. 
+                        </div>
+                    </div>
+                </div>
+                <div className="accordion-item">
+                    <h2 className="accordion-header" id="headingThree">
+                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                            Browser compatible
+                        </button>
+                    </h2>
+                    <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                        <div className="accordion-body">
+                    This word counter software works in any browser such as Chrome, Firefox, Opera, Safari, Internet explorer. It suits to count characters in facebook, blogs, books, excel documents, essays, pdf documents etc.
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <button className="btn btn-primary my-3" >Enable dark mode</button>
+        </div>
+    )
+}
